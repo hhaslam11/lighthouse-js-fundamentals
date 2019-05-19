5c92804e06c0a950969dc98403289faecde2d108
Kaleb Haslam <kalebmarc@hotmail.com>
2019-05-19 02:16:58 +0000

    even.js exercise with isEven function

--- even.js
+++ even.js
@@ -1,14 +1,6 @@
-function sayHello(name) {
-  console.log(`Hello, ${name}`);
-}
-
 function isEven(num) {
   return (num % 2) === 0;
 }
-
-sayHello("Kaleb");
-sayHello("Katelyn");
-sayHello("Derek");
 
 console.log(isEven(15));
 console.log(isEven(256));
