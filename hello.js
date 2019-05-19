84397de5763385c0eb2b2fbcfde54fa6b3dcba18
Kaleb Haslam <kalebmarc@hotmail.com>
2019-05-19 23:42:49 +0000

    Added odd.js that checks if a number is odd

--- hello.js
+++ hello.js
@@ -2,15 +2,6 @@ function sayHello(name) {
   console.log(`Hello, ${name}`);
 }
 
-function isEven(num) {
-  return (num % 2) === 0;
-}
-
 sayHello("Kaleb");
 sayHello("Katelyn");
 sayHello("Derek");
-
-console.log(isEven(15));
-console.log(isEven(256));
-console.log(isEven(0));
-console.log(isEven(-16));
