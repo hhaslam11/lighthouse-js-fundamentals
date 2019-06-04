c2ad39d0dc3cfa57c9f029fd3480fc7d64851340
Kaleb Haslam <kalebmarc@hotmail.com>
2019-06-04 21:33:25 +0000

    added looping range project

---
+++ looping-range.js
@@ -0,0 +1,19 @@
+/*
+ * The function takes 3 integer parameters: start, end, and step.
+ * The function should return an array of numbers from start to end counting by step.
+ */
+
+function range(start, end, step) {
+  if (step < 0) return [];
+  let completedArray = [];
+  for (let i = start; i <= end; i += step) {
+    completedArray.push(i);
+  }
+  return completedArray;
+}
+
+
+//tests
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
